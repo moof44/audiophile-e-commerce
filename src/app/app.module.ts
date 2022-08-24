ed76e52bfc05c56a17eae56f0c007fc9a30f2388
jireh <moof44@yahoo.com>
2022-08-25 02:18:02 +0800

    Finished (1) Create skeletal user interface component design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './features/layout/header/header.component';
+import { FooterComponent } from './features/layout/footer/footer.component';
+import { HomeComponent } from './features/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
