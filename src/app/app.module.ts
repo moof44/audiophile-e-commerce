803d2309401834897fe5b0f74ef93fa9db3801ce
jireh <moof44@yahoo.com>
2022-08-25 02:48:15 +0800

    Save initial coloring of header,content,footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './features/layout/header/header.component';
 import { FooterComponent } from './features/layout/footer/footer.component';
 import { HomeComponent } from './features/pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './features/pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
